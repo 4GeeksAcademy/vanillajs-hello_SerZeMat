59227dd0741c4660731e4abfce5dfe35e55aff31
SerZeMat <secemacorp@gmail.com>
2024-09-06 12:34:27 +0000

    third commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
+
+let generateExcuse = function() {
+  let who = ["El perro", "Mi abuela", "El cartero", "Mi tortuga"];
+  let action = ["se comió", "meó", "aplastó", "rompió"];
+  let what = ["mis deberes", "mi teléfono", "el coche"];
+  let when = [
+    "antes de la clase",
+    "mientras dormía",
+    "mientras hacía ejercicio",
+    "durante mi comida",
+    "mientras estaba conduciendo"
+  ];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
